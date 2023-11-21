f787668e64be05b84d377deec94709756cbf7b89
AsminSayim <145451871+AsminSayim@users.noreply.github.com>
2023-11-21 11:01:08 +0300

    Add files via upload

---
+++ class-ornek/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; 
+
+namespace class_ornek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Kitap kitap= new Kitap();
+            kitap.isim = "Gökyüzü";
+            kitap.yazar = "Ahmet Cansever";
+            kitap.sayfaSayisi = 300;
+            kitap.tür = "Edebiyat";
+            kitap.yili = 2022;
+            label1.Text = kitap.isim +" "+ kitap.yazar + " "+ kitap.tür;
+
+        }
+    }
+}
---
+++ class-ornek/Kitap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_ornek
+{
+    public class Kitap
+    {
+        public string isim;
+        public string yazar;
+        public int sayfaSayisi;
+        public string tür;
+        public int yili;
+    }
+}
